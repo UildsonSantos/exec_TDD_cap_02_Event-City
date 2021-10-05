b756936609592bd30e28580de80a4e5cb5b1bb0a
Uildson S. dos Santos <wilder_shon@hotmail.com>
2021-10-05 06:49:00 -0300

    Created a new workspace for the initial backend

---
+++ src/main/java/com/devsuperior/exec/tdd/cap02/eventcity/MyEventCityApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.exec.tdd.cap02.eventcity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyEventCityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyEventCityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/exec/tdd/cap02/eventcity/MyEventCityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.exec.tdd.cap02.eventcity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyEventCityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
